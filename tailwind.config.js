9247275b3d76b8c437fcc57e9d1db942f8236a36
Alexey Zhokhov <alexey@zhokhov.com>
2022-03-23 02:44:14 +0800

    First block on main page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        riptide: "#8EEDD2",
+        "caribbean-green": "#00CC81",
+      },
+      fontFamily: {
+        dmsans: ["DM Sans"],
+        manrope: ["Manrope"],
+      },
+    },
   },
   plugins: [],
-}
+};
